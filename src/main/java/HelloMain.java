173c0e700a8c1ab1062f720527a28894ac3eea51
coskunccc <coskunarslan@yahoo.com>
2022-08-10 14:01:56 +0300

    New change added

--- src/main/java/HelloMain.java
+++ src/main/java/HelloMain.java
@@ -2,5 +2,6 @@ public class HelloMain {
     public static void main(String[] args) {
         System.out.println("hello main");
         System.out.println("changed");
+        System.out.println("Changes made to see in github");
     }
 }
